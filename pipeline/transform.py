7d4eb5cf90e6d4a66da92415b35a639ed10aee16
kurt <trainee.kurt.martin-brown@sigmalabs.co.uk>
2024-11-26 10:05:21 +0000

    added errors='COERCE'

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -5,11 +5,15 @@ import pandas as pd
 def clean_plant_data(plant_df: pd.DataFrame) -> pd.DataFrame:
     """Setting the types for dataframe columns and removing NaN values"""
 
-    plant_df['recording_at'] = pd.to_datetime(plant_df['recording_at'])
-    plant_df['last_watered'] = pd.to_datetime(plant_df['last_watered'])
-    plant_df['soil_moisture'] = pd.to_numeric(plant_df['soil_moisture'], errors='COERCE')
-    plant_df['temperature'] = pd.to_numeric(plant_df['temperature'])
-    plant_df['plant_id'] = pd.to_numeric(plant_df['plant_id'])
+    plant_df['recording_at'] = pd.to_datetime(
+        plant_df['recording_at'], errors='COERCE')
+    plant_df['last_watered'] = pd.to_datetime(
+        plant_df['last_watered'], errors='COERCE')
+    plant_df['soil_moisture'] = pd.to_numeric(
+        plant_df['soil_moisture'], errors='COERCE')
+    plant_df['temperature'] = pd.to_numeric(
+        plant_df['temperature'], errors='COERCE')
+    plant_df['plant_id'] = pd.to_numeric(plant_df['plant_id'], errors='COERCE')
     plant_df = plant_df[(plant_df['soil_moisture'] >= 0) &
                         (plant_df['soil_moisture'] <= 100)]
 
