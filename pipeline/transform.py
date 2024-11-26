10e0ae564bf4976504d3abdd68e579f0ef389b0d
kurt <trainee.kurt.martin-brown@sigmalabs.co.uk>
2024-11-26 11:33:50 +0000

    added tests for cleaning data

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -13,7 +13,6 @@ def clean_plant_data(plant_df: pd.DataFrame) -> pd.DataFrame:
     """Setting the types for dataframe columns and removing NaN values"""
 
     plant_df['recording_at'] = pd.to_datetime(
-        << << << < HEAD
         plant_df['recording_at'], errors='coerce')
     plant_df['last_watered'] = pd.to_datetime(
         plant_df['last_watered'], errors='coerce', utc=True).dt.tz_convert(None)
