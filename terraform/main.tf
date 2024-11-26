365d0d62c51fa3b31f9c67c5c7db5a5ace6beec9
kurt <trainee.kurt.martin-brown@sigmalabs.co.uk>
2024-11-26 17:56:45 +0000

    added S3 bucket resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,11 @@ resource "aws_ecr_repository" "c14_team_growth_lmnh" {
 output "ecr_repository_id" {
   value = aws_ecr_repository.c14_team_growth_lmnh.id
 }
+
+resource "aws_s3_bucket" "gamma_long_term_storage" {
+  bucket = "gamma-long-term-storage"                   
+
+  tags = {
+    Name        = "LongTermStorage"
+  }
+}
