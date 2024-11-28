3846e6ccda2419b17feca6d75912962b68729b77
Krishna Kumar Seechurn <kseechurn99@outlook.com>
2024-11-28 10:17:43 +0000

    #3 Created eventbridge schedler for mini etl

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,7 +86,7 @@ resource "aws_scheduler_schedule" "team_growth_every_minute_schedule" {
 
   target {
     arn      = aws_lambda_function.c14_team_growth_lambda.arn
-    role_arn = aws_iam_role.c14-team-growth-lambda-role.arn
+    role_arn = aws_iam_role.c14_team_growth_lambda_role.arn
     input    = jsonencode({
       action = "Invoke Lambda"
     })
@@ -220,5 +220,83 @@ resource "aws_ecs_task_definition" "c14_team_growth_rds_to_s3_etl" {
   }
 }
 
+resource "aws_scheduler_schedule" "pipeline_schedule" {
+  name                = "c14-team-growth-daily-etl" 
+  description         = "Triggers the ECS task every 24 hours at midnight"
+  schedule_expression = "cron(0 0 * * ? *)"
+  flexible_time_window {
+    mode = "OFF"
+  }
 
+  target {
+    arn = "arn:aws:ecs:eu-west-2:129033205317:cluster/c14-ecs-cluster"
+    role_arn = aws_iam_role.eventbridge_role.arn
+    ecs_parameters {
+        task_definition_arn = "arn:aws:ecs:eu-west-2:129033205317:task-definition/c14-team-growth-rds-to-s3-etl:4"
+        launch_type = "FARGATE"  
+        network_configuration {
+            subnets = [
+                "subnet-0497831b67192adc2",
+                "subnet-0acda1bd2efbf3922",
+                "subnet-0465f224c7432a02e"
+            ]
+            security_groups = [aws_security_group.ecs_task_sg.id] 
+            assign_public_ip = true 
+        }
+    }
+  }
+}
 
+# IAM Role for EventBridge to trigger ECS tasks
+resource "aws_iam_role" "eventbridge_role" {
+  name = "EventBridgeEcsInvokeRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "scheduler.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach necessary ECS permissions to the EventBridge role
+resource "aws_iam_role_policy" "eventbridge_ecs_policy" {
+  name = "EventBridgeECSPolicy"
+  role = aws_iam_role.eventbridge_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ecs:RunTask",
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "iam:PassRole"
+        ],
+        Resource = "*"
+      },
+
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+        ],
+        Resource = "arn:aws:ecr:eu-west-2:129033205317:repository/c14-team-growth-lmnh"
+      }
+    ]
+  })
+}
