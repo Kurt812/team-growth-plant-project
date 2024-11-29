45bfbc8450b45c3f2bdeb25c626e2618518363ff
Jakub-Poskrop <jakubposkrop05@gmail.com>
2024-11-29 10:58:11 +0000

    Moved all dockerfiles into their respective files

--- dockerise_pipeline.sh
+++ dockerise_pipeline.sh
@@ -1,6 +1,6 @@
 read -p "Enter the repository name: " REPOSITORY_NAME
 source .env
-docker build --file minute_pipeline_dockerfile -t team_growth_pipeline:v1.0 . --platform "linux/amd64"
+docker build --file pipeline/minute_pipeline_dockerfile -t team_growth_pipeline:v1.0 . --platform "linux/amd64" 
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.eu-west-2.amazonaws.com
 if [[ $1 = "new" ]]; then
     echo "Creating a new repository: $REPOSITORY_NAME"
