06544a52e2cc633fa1acf2bf36d276ec6dea9c97
kurt <trainee.kurt.martin-brown@sigmalabs.co.uk>
2024-11-29 12:29:31 +0000

    services required to host dashboard on ecs added

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -18,3 +18,19 @@ resource "aws_ecr_repository" "c14_team_growth_lmnh" {
 output "ecr_repository_id" {
   value = aws_ecr_repository.c14_team_growth_lmnh.id
 }
+
+resource "aws_ecr_repository" "c14_team_growth_lmnh_dashboard" {
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+
+  image_tag_mutability = "MUTABLE"
+  name                 = "c14-team-growth-lmnh-dashboard"
+  lifecycle {
+    prevent_destroy = false
+  }
+}
